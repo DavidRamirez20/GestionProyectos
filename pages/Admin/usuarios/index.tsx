9106acfd466a82a37553800349cf1b1d2eab27f3
David Ramirez <alejoramirezrios@gmail.com>
2021-11-03 14:00:19 -0500

    Agregar archivo _document, implementar iconos y cambios de FrontEnd

--- pages/Admin/usuarios/index.tsx
+++ pages/Admin/usuarios/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const index = () => {
    return (
       <div>
+         <Head>
+            <title>Admin Usuarios | Gestión Proyectos</title>
+         </Head>
          <p>Página de admin de usuarios</p>
+         <i className='fas fa-home'></i>
          <Link href='/'>
             <a>Ir al Home</a>
          </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>Home | Gestión Proyectos</title>
+      </Head>
       <p>Hola Mundo, Soy Next JS</p>
       <Link href='/Admin/usuarios'>
         <a className='cursor-pointer'>Ir a admin usuarios</a>
